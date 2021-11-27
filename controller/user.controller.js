ef7dbef45e7c916899f4fbe399f4138f7e8225ff
unknown <benaremail@gmail.com>
2021-11-27 15:49:58 +0800

    m

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,9 +3,12 @@ import db from "../config/db.js"
 
 let users = [];
 
-export const getUsers = (req, res)=>{
+export const getUsers = async (req, res)=>{
     
-    res.send(users);
+    const response = await db('users').orderBy('created_at', 'desc');
+    // console.log('response gerusers: ', response);
+    
+    res.send(response);
 
 };
 
@@ -19,7 +22,7 @@ export const createUser = async (req, res)=>{
             email: req.body.email,
             contact: req.body.contact,
         });
-        console.log('insert succes: ', response);
+        // console.log('insert succes: ', response);
         res.send('User Added Successfully');
     } catch (error) {
         console.log('error: ', error);
@@ -27,23 +30,36 @@ export const createUser = async (req, res)=>{
     
 };
 
-export const getUserByID = (req, res)=>{
+export const getUserByID = async (req, res)=>{
     const singleUser = users.filter((user)=>user.id === req.params.id);
-    res.send(singleUser);
+    const singleUserReponse = await db('users').where('id', req.params.id)
+    res.send(singleUserReponse);
 }
 
-export const deleteUser = (req, res)=>{
-    users = users.filter((user)=>user.id !== req.params.id);
-    res.send('user deleted');
+export const deleteUser = async (req, res)=>{
+    // users = users.filter((user)=>user.id !== req.params.id);
+    try {
+        const deleteUserResponse = await db('users').where('id', req.params.id).del();
+         res.send('user deleted');
+    } catch (error) {
+        console.log('error deleting: ', error);
+    }
+    
 }
 
-export const updateUser = (req, res)=>{
-    const user = users.find((user)=>user.id === req.params.id);
+export const updateUser = async (req, res)=>{
+    // const user = users.find((user)=>user.id === req.params.id);
+
+    const userUpdateResponse = await db('users').where('id', req.params.id).update({
+        name: req.body.name,
+        email: req.body.email,
+        contact: req.body.contact,
+    });
 
     // console.log(user);
-    user.name = req.body.name;
-    user.contact = req.body.contact;
-    user.email = req.body.email;
+    // user.name = req.body.name;
+    // user.contact = req.body.contact;
+    // user.email = req.body.email;
 
     res.send('User Updated');
 }
